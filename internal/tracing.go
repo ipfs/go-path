a3cee51404fb27bc1640243775af563d78f0dee2
Henrique Dias <hacdias@gmail.com>
2023-05-18 10:50:03 +0200

    chore: deprecate and readme

--- internal/tracing.go
+++ internal/tracing.go
@@ -8,6 +8,7 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+// Deprecated: use github.com/ipfs/boxo/path/internal.StartSpan
 func StartSpan(ctx context.Context, name string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
 	return otel.Tracer("go-path").Start(ctx, fmt.Sprintf("Path.%s", name), opts...)
 }
